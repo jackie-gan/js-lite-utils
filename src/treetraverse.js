e3e7b543045498ee007a40ec484450601ce38104
jackie-gan <jackiegan405679096@gmail.com>
2020-07-26 11:38:18 +0800

    feat: add dfs and bfs

--- src/treetraverse.js
+++ src/treetraverse.js
@@ -1,5 +1,7 @@
 /**
  * 非递归先序遍历
+ * 
+ * node { left, value, right }
  */
 export function preorderTraverse(node) {
   const nodes = [];
@@ -19,6 +21,8 @@ export function preorderTraverse(node) {
 
 /**
  * 非递归中序遍历
+ * 
+ * node { left, value, right }
  */
 export function inorderTraverse(node) {
   const nodes = [];
@@ -38,6 +42,8 @@ export function inorderTraverse(node) {
 
 /**
  * 非递归后序遍历
+ * 
+ * node { left, value, right }
  */
 export function postorderTraverse(node) {
   const nodes = [];
@@ -53,4 +59,43 @@ export function postorderTraverse(node) {
     }
   }
   return visitNodes.reverse();
+}
+
+/**
+ * 深度优先遍历
+ * 
+ * node { children, value }
+ */
+export function dfs(node) {
+  const stack = [];
+  const visitNodes = [];
+  stack.push(node);
+  while (stack.length > 0) {
+    const n = stack.pop();
+    visitNodes.push(n.value);
+    const c = n.children;
+    for (let i = c.length - 1; i >= 0; i--) {
+      stack.push(c[i]);
+    }
+  }
+
+  return visitNodes;
+}
+
+/**
+ * 广度优先遍历
+ * 
+ * node { children, value }
+ */
+export function bfs(node) {
+  const stack = [];
+  const visitNodes = [];
+  stack.push(node);
+  while (stack.length > 0) {
+    const n = stack.shift();
+    visitNodes.push(n.value);
+    const c = n.children;
+    [].push.apply(stack, c);
+  }
+  return visitNodes;
 }
